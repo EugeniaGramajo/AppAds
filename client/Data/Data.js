8e48180c4e608efdb6a853a5873c7e2877bbada7
EusebioLujan <narboleta@gmail.com>
2023-04-24 09:59:12 -0300

    Component/logingFrom/3

---
+++ client/Data/Data.js
@@ -0,0 +1,10 @@
+export  const user={
+    name:"Carlos",
+    lastname:"Bodoque",
+    email:"superadmin@gmail.com",
+    password:"superAdmin",
+    points:0,
+    views:0,
+    premium:false,
+id:"3459t8jh0ui"
+}
---
+++ client/components/User/LogingForm/LoginForm.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet , Text,Pressable} from 'react-native';
+import { user } from '../../../Data/Data';
+
+const LoginForm = () => {
+  const [email, setEmail, ] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Lógica de inicio de sesión aquí
+    if (email === user.email && password === user.password) {
+      alert('Inicio de sesión exitoso');
+      setEmail("");
+      setPassword("")
+    } else {
+      alert('Credenciales incorrectas');
+      setEmail("");
+      setPassword("")
+    }
+  };
+
+  return (
+    <View className="flex  w-9/12 h-36 items-center">
+        <Text className="text-xl text-white m-2">Bienvenidos</Text>
+      <TextInput
+        className="bg-gray-100 text-black h-2/6 rounded-xl w-11/12 p-2 m-1 shadow-2xl shadow-white"
+        placeholder="Email"
+        onChangeText={setEmail}
+        value={email}
+      />
+      <TextInput
+        className="bg-gray-100 text-black h-2/6 rounded-xl w-11/12 p-2 m-1a shadow-2xl shadow-white"
+        placeholder="Password"
+        onChangeText={setPassword}
+        value={password}
+        secureTextEntry
+      />
+      <Pressable className="bg-buttonBlue h-2/6 w-1/2 items-center rounded-2xl m-1 shadow-2xl shadow-buttonBlue" onPress={handleLogin}>
+        <Text className="flex justify-center items-center m-auto">Sign In</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default LoginForm;
