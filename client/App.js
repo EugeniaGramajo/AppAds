8de5b6c1d5f0337790a736d00b15d046fb71cbb4
EusebioLujan <narboleta@gmail.com>
2023-04-27 15:39:01 -0300

    feat:(app/newstack container)fix:(componentes/home and user)

--- client/App.js
+++ client/App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import Navigation from "./components/User/Navigation/Navigation";
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import LoginForm from "./components/User/LogingForm/LoginForm";
+import RegisterForm from "./components/User/LogingForm/RegisterForm";
+import RecoveryPassword from "./components/User/LogingForm/RecoveryPasswordForm";
+import HomeScreen from "./components/Home/HomeScreen";
+
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
     <>
-       <Navigation/> 
+       <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginForm" screenOptions={{ headerShown: false, }}>
+        <Stack.Screen name="LoginForm" component={LoginForm} />
+        <Stack.Screen name="RegisterForm" component={RegisterForm} />
+        <Stack.Screen name="RecoveryPasswordForm" component={RecoveryPassword} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
     </>
   );
 }
