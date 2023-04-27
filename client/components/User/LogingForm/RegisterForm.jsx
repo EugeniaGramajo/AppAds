8bea67e75bad9a28a422389e8c217c660f42422e
EusebioLujan <narboleta@gmail.com>
2023-04-27 15:28:18 -0300

    a

--- client/components/User/LogingForm/RegisterForm.jsx
+++ client/components/User/LogingForm/RegisterForm.jsx
@@ -6,6 +6,7 @@ export default function RegisterForm() {
     const [email, setEmail, ] = useState('');
     const [password, setPassword] = useState('');
     const auth = getAuth();
+    
     const handleRegister = () => {
       createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
